1c91f683b2660e28e6a4050ec60883c49ea9b46f
unknown <zaid@inspedium.com>
2021-03-30 11:53:12 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,672 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test Project 1</title>
+	<style type="text/css">
+	.myimage {
+    width: 1500px;
+    height: 300px;
+    position: relative;
+}
+.face {
+    width: 60px;
+    height: 90px;
+    border: 1px solid black;
+    top: 70px;
+    left: 70px;
+    position: absolute
+}
+.face2 {
+    width: 60px;
+    height: 90px;
+    border: 1px solid black;
+    top: 70px;
+    left: 70px;
+    position: absolute
+}
+
+.commercialbox {
+    border: 1px solid black;
+    width: 35px;
+    height: 35px;
+    position: absolute
+}
+
+.residentialboxA {
+    border: 1px solid black;
+    width: 55px;
+    height: 25px;
+    position: absolute
+}
+
+.residentialboxC {
+    border: 1px solid black;
+    width: 82px;
+    height: 44px;
+    position: absolute
+}
+
+.residentialboxC_custom {
+    border: 1px solid black;
+    width: 44px;
+    height: 82px;
+    position: absolute
+}
+
+.residentialboxC_custom2 {
+    border: 1px solid black;
+    width: 65px;
+    height: 56px;
+    position: absolute
+}
+
+.residentialboxA_custom {
+    border: 1px solid black;
+    width: 35px;
+    height: 38px;
+    position: absolute
+}
+
+.com20 {
+    top: 172px;
+    left: 295px;
+}
+
+.com19 {
+    top: 185px;
+    left: 337px;
+}
+
+.com18 {
+    top: 198px;
+    left: 375px;
+}
+
+.com17 {
+    top: 209px;
+    left: 410px;
+}
+
+.com16 {
+    top: 218px;
+    left: 448px;
+}
+
+.com15 {
+    top: 230px;
+    left: 485px;
+}
+.com14 {
+    top: 240px;
+    left: 520px;
+}
+.com13 {
+    top: 250px;
+    left: 560px;
+}
+.com12 {
+    top: 262px;
+    left: 595px;
+}
+.com11 {
+    top: 274px;
+    left: 637px;
+}
+
+.com10 {
+    top: 300px;
+    left: 725px;
+}
+.com9 {
+    top: 315px;
+    left: 768px;
+}
+.com8 {
+    top: 328px;
+    left: 805px;
+}
+.com7 {
+    top: 337px;
+    left: 840px;
+}
+.com5 {
+    top: 362px;
+    left: 925px;
+}
+.com6 {
+    top: 350px;
+    left: 880px;
+}
+.com4 {
+    top: 375px;
+    left: 963px;
+}
+.com3 {
+    top: 385px;
+    left: 1000px;
+}
+.com2 {
+    top: 395px;
+    left: 1037px;
+}
+.com1 {
+    top: 410px;
+    left: 1080px;
+}
+
+/*Residential Plots*/
+.res1 {
+    top: 612px;
+    left: 1068px;
+}
+.res2 {
+    top: 584px;
+    left: 1068px;
+}
+.res3 {
+    top: 555px;
+    left: 1068px;
+}
+.res4 {
+    top: 527px;
+    left: 1068px;
+}
+.res5 {
+    top: 498px;
+    left: 1068px;
+}
+.res6 {
+    top: 498px;
+    left: 1011px;
+}
+.res7 {
+    top: 527px;
+    left: 1011px;
+}
+.res8 {
+    top: 555px;
+    left: 1011px;
+}
+.res9 {
+    top: 584px;
+    left: 1011px;
+}
+.res10 {
+    top: 612px;
+    left: 1011px;
+}
+.res11 {
+    top: 614px;
+    left: 924px;
+}
+.res12 {
+    top: 586px;
+    left: 924px;
+}
+.res13 {
+    top: 556px;
+    left: 923px;
+}
+.res14 {
+    top: 528px;
+    left: 923px;
+}
+.res15 {
+    top: 498px;
+    left: 923px;
+}
+.res16 {
+    top: 470px;
+    left: 923px;
+}
+.res22 {
+    top: 614px;
+    left: 866px;
+}
+.res21 {
+    top: 586px;
+    left: 866px;
+}
+.res20 {
+    top: 556px;
+    left: 866px;
+}
+.res19 {
+    top: 528px;
+    left: 866px;
+}
+.res18 {
+    top: 498px;
+    left: 866px;
+}
+.res17 {
+    top: 470px;
+    left: 866px;
+}
+.res24 {
+    top: 573px;
+    left: 778px;
+}
+.res23 {
+    top: 601px;
+    left: 798px;
+}
+.res25 {
+    top: 543px;
+    left: 778px;
+}
+.res26 {
+    top: 514px;
+    left: 778px;
+}
+.res27 {
+    top: 485px;
+    left: 778px;
+}
+.res28 {
+    top: 456px;
+    left: 778px;
+}
+.res29 {
+    top: 427px;
+    left: 778px;
+}
+.res35 {
+    top: 573px;
+    left: 721px;
+}
+.res36 {
+    top: 601px;
+    left: 721px;
+}
+.res37 {
+    top: 601px;
+    left: 759px;
+}
+.res34 {
+    top: 543px;
+    left: 721px;
+}
+.res33 {
+    top: 514px;
+    left: 721px;
+}
+.res32 {
+    top: 485px;
+    left: 721px;
+}
+.res31 {
+    top: 456px;
+    left: 721px;
+}
+.res30 {
+    top: 427px;
+    left: 721px;
+}
+
+/*C Block*/
+.res_C1 {
+    top: 454px;
+    left: 595px;
+}
+.res_C2 {
+    top: 405px;
+    left: 594px;
+}
+.res_C3 {
+    top: 358px;
+    left: 594px;
+}
+.res_C4 {
+    top: 358px;
+    left: 508px;
+}
+.res_C5 {
+    top: 405px;
+    left: 508px;
+}
+.res_C6 {
+    top: 454px;
+    left: 508px;
+}
+
+.res_C7 {
+    top: 600px;
+    left: 383px;
+}
+.res_C8 {
+    top: 552px;
+    left: 383px;
+}
+.res_C9 {
+    top: 504px;
+    left: 382px;
+}
+.res_C10 {
+    top: 455px;
+    left: 382px;
+}
+.res_C11 {
+    top: 408px;
+    left: 381px;
+}
+.res_C12 {
+    top: 359px;
+    left: 381px;
+}
+
+.res_C18 {
+    top: 600px;
+    left: 296px;
+}
+.res_C17 {
+    top: 552px;
+    left: 296px;
+}
+.res_C16 {
+    top: 504px;
+    left: 296px;
+}
+.res_C15 {
+    top: 455px;
+    left: 296px;
+}
+.res_C14 {
+    top: 408px;
+    left: 296px;
+}
+.res_C13 {
+    top: 359px;
+    left: 296px;
+}
+
+.res_C19 {
+    top: 603px;
+    left: 170px;
+}
+.res_C20 {
+    top: 555px;
+    left: 170px;
+}
+.res_C21 {
+    top: 507px;
+    left: 170px;
+}
+.res_C22 {
+    top: 458px;
+    left: 170px;
+}
+.res_C23 {
+    top: 410px;
+    left: 169px;
+}
+.res_C24 {
+    top: 361px;
+    left: 169px;
+}
+
+.res_C30 {
+    top: 603px;
+    left: 85px;
+}
+.res_C29 {
+    top: 555px;
+    left: 84px;
+}
+.res_C28 {
+    top: 507px;
+    left: 83px;
+}
+.res_C27 {
+    top: 458px;
+    left: 83px;
+}
+.res_C26 {
+    top: 410px;
+    left: 82px;
+}
+.res_C25 {
+    top: 361px;
+    left: 83px;
+}
+
+.res_C31 {
+    top: 681px;
+    left: 970px;
+}
+.res_C32 {
+    top: 681px;
+    left: 1039px;
+}
+.res_C33 {
+    top: 681px;
+    left: 1088px;
+}
+.res_C34 {
+    top: 679px;
+    left: 1136px;
+}
+.res_C35 {
+    top: 679px;
+    left: 1184px;
+}
+.res_C36 {
+    top: 678px;
+    left: 1230px;
+}
+.res_C37 {
+    top: 761px;
+    left: 1230px;
+}
+
+.res_C41 {
+    top: 767px;
+    left: 1039px;
+}
+.res_C40 {
+    top: 767px;
+    left: 1088px;
+}
+.res_C42 {
+    top: 801px;
+    left: 970px;
+}
+.res_C43 {
+    top: 742px;
+    left: 970px;
+}
+.res_C39 {
+    top: 765px;
+    left: 1136px;
+}
+.res_C38 {
+    top: 763px;
+    left: 1184px;
+}
+
+.foot {
+    width: 60px;
+    height: 90px;
+    border: 1px solid green;
+    top: 110px;
+    left: 200px;
+    position: absolute
+}
+img {
+    width: 100%;
+    height: auto;
+}
+.face:hover {
+    background-color: red;
+}
+.face2:hover {
+    background-color: yellow;
+}
+
+.commercialbox:hover, .residentialboxA:hover, .residentialboxA_custom:hover, .residentialboxC:hover, .residentialboxC_custom:hover, .residentialboxC_custom2:hover {
+    background-color: red;
+    opacity: 0.5;
+}
+
+/*.com20:hover {
+    background-color: blue;
+}
+.com19:hover {
+    background-color: blue;
+}
+.com18:hover {
+    background-color: blue;
+}
+.com17:hover {
+    background-color: blue;
+}
+.com16:hover {
+    background-color: blue;
+}
+.com16:hover {
+    background-color: blue;
+}
+.com16:hover {
+    background-color: blue;
+}*/
+
+.foot:hover {
+    background-color: green;
+}
+</style>
+</head>
+<body>
+<div class='myimage'>
+    <img src='sitemap_1.jpg' />
+    <?php
+    // $booked = 1;
+    
+    // if($booked == 0){
+    // 	echo "<div class='face'></div>";
+    // }else{
+    // 	echo "<div class='face2'></div>";
+    // }
+
+    ?>
+    <!-- SA Commercial 11-20 -->
+    <div class='commercialbox com20'></div>
+    <div class='commercialbox com19'></div>
+    <div class='commercialbox com18'></div>
+    <div class='commercialbox com17'></div>
+    <div class='commercialbox com16'></div>
+    <div class='commercialbox com15'></div>
+    <div class='commercialbox com14'></div>
+    <div class='commercialbox com13'></div>
+    <div class='commercialbox com12'></div>
+    <div class='commercialbox com11'></div>
+    <!-- SA Commercial 11-20 end-->
+
+
+    <!-- SA Commercial 1-10 -->
+    <div class='commercialbox com10'></div>
+    <div class='commercialbox com9'></div>
+    <div class='commercialbox com8'></div>
+    <div class='commercialbox com7'></div>
+    <div class='commercialbox com6'></div>
+    <div class='commercialbox com5'></div>
+    <div class='commercialbox com4'></div>
+    <div class='commercialbox com3'></div>
+    <div class='commercialbox com2'></div>
+    <div class='commercialbox com1'></div> 
+
+    <!-- Block A -->
+    <div class='residentialboxA res1'></div>
+    <div class='residentialboxA res2'></div>
+    <div class='residentialboxA res3'></div>
+    <div class='residentialboxA res4'></div>
+    <div class='residentialboxA res5'></div>
+    <div class='residentialboxA res6'></div>
+    <div class='residentialboxA res7'></div>
+    <div class='residentialboxA res8'></div>
+    <div class='residentialboxA res9'></div>
+    <div class='residentialboxA res10'></div>
+
+    <div class='residentialboxA res11'></div>
+    <div class='residentialboxA res12'></div>
+    <div class='residentialboxA res13'></div>
+    <div class='residentialboxA res14'></div>
+    <div class='residentialboxA res15'></div>
+    <div class='residentialboxA res16'></div>
+    <div class='residentialboxA res17'></div>
+    <div class='residentialboxA res18'></div>
+    <div class='residentialboxA res19'></div>
+    <div class='residentialboxA res20'></div>
+    <div class='residentialboxA res21'></div>
+    <div class='residentialboxA res22'></div>
+    
+    <div class='residentialboxA_custom res23'></div>
+    <div class='residentialboxA res24'></div>
+    <div class='residentialboxA res25'></div>
+    <div class='residentialboxA res26'></div>
+    <div class='residentialboxA res27'></div>
+    <div class='residentialboxA res28'></div>
+    <div class='residentialboxA res29'></div>
+    <div class='residentialboxA res30'></div>
+    <div class='residentialboxA res31'></div>
+    <div class='residentialboxA res32'></div>
+    <div class='residentialboxA res33'></div>
+    <div class='residentialboxA res34'></div>
+    <div class='residentialboxA res35'></div>
+    <div class='residentialboxA_custom res36'></div>
+    <div class='residentialboxA_custom res37'></div>
+    <!-- Block A End -->
+
+    <!-- Block C -->
+    <div class='residentialboxC res_C1'></div>
+    <div class='residentialboxC res_C2'></div>
+    <div class='residentialboxC res_C3'></div>
+    <div class='residentialboxC res_C4'></div>
+    <div class='residentialboxC res_C5'></div>
+    <div class='residentialboxC res_C6'></div>
+
+    <div class='residentialboxC res_C7'></div>
+    <div class='residentialboxC res_C8'></div>
+    <div class='residentialboxC res_C9'></div>
+    <div class='residentialboxC res_C10'></div>
+    <div class='residentialboxC res_C11'></div>
+    <div class='residentialboxC res_C12'></div>
+    <div class='residentialboxC res_C13'></div>
+    <div class='residentialboxC res_C14'></div>
+    <div class='residentialboxC res_C15'></div>
+    <div class='residentialboxC res_C16'></div>
+    <div class='residentialboxC res_C17'></div>
+    <div class='residentialboxC res_C18'></div>
+
+    <div class='residentialboxC res_C19'></div>
+    <div class='residentialboxC res_C20'></div>
+    <div class='residentialboxC res_C21'></div>
+    <div class='residentialboxC res_C22'></div>
+    <div class='residentialboxC res_C23'></div>
+    <div class='residentialboxC res_C24'></div>
+    <div class='residentialboxC res_C25'></div>
+    <div class='residentialboxC res_C26'></div>
+    <div class='residentialboxC res_C27'></div>
+    <div class='residentialboxC res_C28'></div>
+    <div class='residentialboxC res_C29'></div>
+    <div class='residentialboxC res_C30'></div>
+
+    <div class='residentialboxC_custom2 res_C31'></div>
+    <div class='residentialboxC_custom res_C32'></div>
+    <div class='residentialboxC_custom res_C33'></div>
+    <div class='residentialboxC_custom res_C34'></div>
+    <div class='residentialboxC_custom res_C35'></div>
+    <div class='residentialboxC_custom res_C36'></div>
+    <div class='residentialboxC_custom res_C37'></div>
+    <div class='residentialboxC_custom res_C38'></div>
+    <div class='residentialboxC_custom res_C39'></div>
+    <div class='residentialboxC_custom res_C40'></div>
+    <div class='residentialboxC_custom res_C41'></div>
+    <div class='residentialboxC_custom2 res_C42'></div>
+    <div class='residentialboxC_custom2 res_C43'></div>
+
+    <!-- <div class='foot'></div> -->
+</div>
+</body>
+</html>
+
+
